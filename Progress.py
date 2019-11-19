a88fc87898dd02f3fc361358c0e71f454de00b15
DOI Takanobu <kumanotakanobu@outlook.jp>
2019-11-19 17:02:45 +0900

    to update bars running the program

--- Progress.py
+++ Progress.py
@@ -12,13 +12,13 @@ while True:
         UserScore = []
     for i in range(len(UserList)):
         Score = 0
-        with open(ProgressFileName+str(i)
+        with open(ProgressFileName
                   +"{0:02d}.txt".format(i+1)) as f:
             for s_line in f:            
-                Score = Score + int(s_line.split()[1])
+                if len(s_line.split())>1:
+                    Score = Score + int(s_line.split()[1])
         UserScore.append(Score)
 
-    plt.bar(np.array(range(1, len(UserList))), np.array(UserScore),
-            tick_label=UserList, align="center")
-    plt.show()
-    sleep(60)
+    plt.bar(np.array(range(1, len(UserList)+1)), np.array(UserScore),
+            tick_label=UserList, align="center", color="b")
+    plt.pause(1)
